c7c0880b2065fa56aace47b37d8d702abd4d768c
koushikkondepati77 <koushikkondepati@gmail.com>
2023-11-06 13:23:20 -0600

    reverted changes due to hardhat config errors

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,22 +3,4 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.19",
-<<<<<<< HEAD
-  networks: {
-    custom: {
-      chainId: 1337, // Change to your desired chain ID
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-      },
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`, // Change to your desired forking URL
-        blockNumber: 13004111, // Block number to fork from
-      },
-      initialBaseFeePerGas: 0, // Set the initial base fee per gas
-      startTime: 1641470400, // Set custom start time
-      initialBlockNumber: 1000, // Set custom initial block number
-    },
-  },
-=======
->>>>>>> 644d24a8ceee1b4b2d4ed07e7f3440a7aab5f94c
 };
