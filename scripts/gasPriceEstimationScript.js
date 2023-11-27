262b4e986d3befae60f3fceb8aa24c24a3e9c863
koushikkondepati77 <koushikkondepati@gmail.com>
2023-11-27 17:38:32 -0600

    commiting changes made in two of the issues

--- scripts/gasPriceEstimationScript.js
+++ scripts/gasPriceEstimationScript.js
@@ -1,11 +1,15 @@
 // gasPriceEstimationScript.js
 // This script uses Hardhat and ethers.js to estimate the maxPriorityFeePerGas.
-
+const { ethers } = require("hardhat");
 async function main() {
     const [signer] = await ethers.getSigners();
     const feeData = await ethers.provider.getFeeData(); // Use eth_maxPriorityFeePerGas
+    console.log(feeData);
   
     console.log("Max Priority Fee Per Gas:", feeData.maxPriorityFeePerGas.toString());
+
+    const gasPrice = await ethers.provider.getGasPrice();
+    console.log("Gas Price:", gasPrice.toString());
   
     if (feeData.effectiveBaseFee !== undefined) {
       console.log("Effective Base Fee:", feeData.effectiveBaseFee.toString());
